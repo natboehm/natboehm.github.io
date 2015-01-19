51b6ef4602bec36141209032cedb40260be733df
Natalie Boehm <nmb56@pitt.edu>
2015-01-19 01:47:51 -0500

    adding index.js, index.css files

---
+++ index.js
@@ -0,0 +1,12 @@
+var slowLorisImg = document.querySelector('img');
+
+slowLorisImg.onclick = function() {
+	var imageSrc = slowLorisImg.getAttribute('src');
+	
+	if (imageSrc === 'http://i1.walesonline.co.uk/incoming/article1785459.ece/alternates/s2197/a-slow-loris-504608477.jpg') {
+	slowLorisImg.setAttribute ('src','http://img3.wikia.nocookie.net/__cb20141118173223/creepypastaitalia/it/images/7/7f/Rake-0.jpg');
+	document.getElementById('goatScream').play();
+	} else {
+		slowLoris.setAttribute ('src','http://i1.walesonline.co.uk/incoming/article1785459.ece/alternates/s2197/a-slow-loris-504608477.jpg');
+	}
+}
