fb0112245fdf1860a5a5cd06bae716adccc3cd6c
Natalie Boehm <nmb56@pitt.edu>
2015-01-21 12:27:24 -0500

    broken

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 var slowLorisImg = document.querySelector('img');
+
 var goatScreamAud = document.getElementById('goatScream'); 
 
 function playGoat() {
 	goatScreamAud.play(); 
 }
 
+function stopGoat() {
+	goatScreamAud.stop();
+}
+
 slowLorisImg.onclick = function() {
 	var imageSrc = slowLorisImg.getAttribute('src');
-	var clicked = false; 
 
 	if (imageSrc === 'http://i1.walesonline.co.uk/incoming/article1785459.ece/alternates/s2197/a-slow-loris-504608477.jpg') {
 		slowLorisImg.setAttribute ('src','http://img3.wikia.nocookie.net/__cb20141118173223/creepypastaitalia/it/images/7/7f/Rake-0.jpg'); 
-		clicked = true; 
-
-		while (clicked)	{
-			playGoat();
-		}
+		playGoat();
 
 	} else {
 		slowLorisImg.setAttribute ('src','http://i1.walesonline.co.uk/incoming/article1785459.ece/alternates/s2197/a-slow-loris-504608477.jpg');
-		clicked = false;  
+		stopGoat();   
 	}
 }
