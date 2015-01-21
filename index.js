58d2296ccbb9b7ef517b9a2ef530ce18de4d4833
Natalie Boehm <nmb56@pitt.edu>
2015-01-21 12:19:44 -0500

    technical difficulties

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function playGoat() {
 
 slowLorisImg.onclick = function() {
 	var imageSrc = slowLorisImg.getAttribute('src');
-	var clicked = true; 
+	var clicked = false; 
 
 	if (imageSrc === 'http://i1.walesonline.co.uk/incoming/article1785459.ece/alternates/s2197/a-slow-loris-504608477.jpg') {
 		slowLorisImg.setAttribute ('src','http://img3.wikia.nocookie.net/__cb20141118173223/creepypastaitalia/it/images/7/7f/Rake-0.jpg'); 
