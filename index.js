3b9d2b1e29159877995ed6a9a12df7b5b7c1c821
Natalie Boehm <nmb56@pitt.edu>
2015-01-22 23:15:11 -0500

    image swatch

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ function stopGoat() {
 setTimeout("changeImage()", 8000);
 
 function changeImage() {
-	document.getElementById("slowImage").src = 'http://img3.wikia.nocookie.net/__cb20141118173223/creepypastaitalia/it/images/7/7f/Rake-0.jpg';	
+	document.getElementById('imageSrc').src = "http://img3.wikia.nocookie.net/__cb20141118173223/creepypastaitalia/it/images/7/7f/Rake-0.jpg";
 	playGoat();
+	slowLorisImg.onclick = slowLorisClicker();
 }
 
-slowLorisImg.onclick = function() {
+ function slowLorisClicker() {
 	if (imageSrc === 'http://i1.walesonline.co.uk/incoming/article1785459.ece/alternates/s2197/a-slow-loris-504608477.jpg') {
 		slowLorisImg.setAttribute ('src','http://img3.wikia.nocookie.net/__cb20141118173223/creepypastaitalia/it/images/7/7f/Rake-0.jpg'); 
 		playGoat(); 
@@ -25,4 +26,4 @@ slowLorisImg.onclick = function() {
 		slowLorisImg.setAttribute ('src','http://i1.walesonline.co.uk/incoming/article1785459.ece/alternates/s2197/a-slow-loris-504608477.jpg');
 		stopGoat();   
 	}
-}
+} 
