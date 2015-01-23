d4b8b5a88cfb0d147c5859ce9bf87bf7eab56238
Natalie Boehm <nmb56@pitt.edu>
2015-01-22 23:34:16 -0500

    image clicks

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var slowLorisImg = document.querySelector('img');
 var goatScreamAud = document.getElementById('goatScream'); 
-var imageSrc = slowLorisImg.getAttribute('src');
 
 function playGoat() {
 	goatScreamAud.play(); 
@@ -20,6 +19,9 @@ function changeImage() {
 
  function slowLorisClicker() {
 	console.log("Slow loris");
+
+	var imageSrc = slowLorisImg.getAttribute('src');	
+
 	if (imageSrc === 'http://i1.walesonline.co.uk/incoming/article1785459.ece/alternates/s2197/a-slow-loris-504608477.jpg') {
 		slowLorisImg.setAttribute ('src','http://img3.wikia.nocookie.net/__cb20141118173223/creepypastaitalia/it/images/7/7f/Rake-0.jpg'); 
 		playGoat(); 
