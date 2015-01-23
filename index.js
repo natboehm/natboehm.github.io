d96129a7ac4aaf1d66979ecb40838794b7390825
Natalie Boehm <nmb56@pitt.edu>
2015-01-22 22:50:44 -0500

    lawlz

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var slowLorisImg = document.querySelector('img');
 
 var goatScreamAud = document.getElementById('goatScream'); 
 
+var imageSrc = slowLorisImg.getAttribute('src');
+
 function playGoat() {
 	goatScreamAud.play(); 
 }
@@ -10,14 +12,22 @@ function stopGoat() {
 	goatScreamAud.pause();
 }
 
+setTimeout("changeImage()", 10000);
+
+function changeImage() {
+	document.getElementById("imgSrc").src = 'http://img3.wikia.nocookie.net/__cb20141118173223/creepypastaitalia/it/images/7/7f/Rake-0.jpg';	
+	playGoat();
+}
+
 slowLorisImg.onclick = function() {
-	var imageSrc = slowLorisImg.getAttribute('src');
 
 	if (imageSrc === 'http://i1.walesonline.co.uk/incoming/article1785459.ece/alternates/s2197/a-slow-loris-504608477.jpg') {
 		slowLorisImg.setAttribute ('src','http://img3.wikia.nocookie.net/__cb20141118173223/creepypastaitalia/it/images/7/7f/Rake-0.jpg'); 
-		playGoat();
+		playGoat(); 
 	} else {
 		slowLorisImg.setAttribute ('src','http://i1.walesonline.co.uk/incoming/article1785459.ece/alternates/s2197/a-slow-loris-504608477.jpg');
 		stopGoat();   
 	}
+	
+	
 }
